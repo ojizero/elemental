2046471d2faa55fc7f663fe0763cd6b119c6e5bb
ojizero <hello@ojizero.tech>
2025-04-11 18:39:52 +0300

    Corrected Elemental root module

--- lib/elemental.ex
+++ lib/elemental.ex
@@ -1,7 +1,11 @@
 defmodule Elemental do
   def __using__(_opts \\ []) do
     quote do
-      import Elemental.Loader
+      import Elemental.Breadcrumbs
+      import Elemental.Dropdown
+      import Elemental.Loading
+      import Elemental.Select
+      import Elemental.Table
       import Elemental.Tooltip
     end
   end
