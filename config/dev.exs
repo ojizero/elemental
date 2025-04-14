8fd6911e6e1af2b10f0674a5f8c3cf569a99dc83
ojizero <hello@ojizero.tech>
2025-04-15 00:16:35 +0300

    Fix reload on storybook

--- config/dev.exs
+++ config/dev.exs
@@ -13,6 +13,7 @@ config :elemental_storybook, ElementalStorybookWeb.Endpoint,
   live_reload: [
     patterns: [
       ~r"../lib/elemental/.*(ex|heex|js|css)$",
+      ~r"storybook/.*(ex|exs|heex|js|css)$",
       ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$"
     ]
   ]
